0bd9b45d9a32adc3a2f24bcac5e7ba0d44566cd2
grzesiek <grzesiekkedzior@gmail.com>
2021-03-04 22:25:46 +0100

    Initial commit

---
+++ src/main/java/com/mail/spammer/MailSpammerApplication.java
@@ -0,0 +1,13 @@
+package com.mail.spammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSpammerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSpammerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mail/spammer/MailSpammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.spammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSpammerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
